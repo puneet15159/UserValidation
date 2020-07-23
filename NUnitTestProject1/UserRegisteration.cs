666e4795043a74fb568075d6685f87a450f82747
puneet <puneet15159@iiitd.ac.in>
2020-07-23 16:58:32 +0530

    [puneet] Added check for Fisrt name

--- NUnitTestProject1/UserRegisteration.cs
+++ NUnitTestProject1/UserRegisteration.cs
@@ -7,7 +7,7 @@ namespace NUnitTestProject1
     {
         Regex firstNameRegex = new Regex(@"[A-Z]{1}[a-z]{2,}");
         Regex lastNameRegex = new Regex(@"[A-Z]{1}[a-z]{2,}");
-        Regex emailRegex = new Regex(@"^[a-bA-Z]{3,}[0-9]*([.\-\+]{1}([a-z]{3,})?[0-9]*)?@[a-zA-Z]{2,}[.][a-z]{2,}([.][a-z]{2,3})?$");
+        Regex emailRegex = new Regex(@"(^[a-bA-Z]{3,}([0-9]{1,})?([.\-\+]{1}([a-z]{3,})?[0-9]*)?@[a-zA-Z]{2,}[.][a-z]{2,}([.][a-z]{2,3})?$)");
         Regex phoneNumberRegex = new Regex(@"[91]{1} [0-9]{10}");
         //Regex passwordRegex = new Regex(@"[a-zA-Z0-9]{8}");
         //Regex passwordRegex = new Regex(@"(?=.*[A-Z])");
